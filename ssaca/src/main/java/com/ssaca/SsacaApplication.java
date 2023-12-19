09a086ee850f99a9689e97d6a6f527c74acdb409
오건영 <o54711254@ogeon-yeong-ui-MacBookAir.local>
2023-12-19 15:53:47 +0900

    feat : 프로젝트 생성

---
+++ ssaca/src/main/java/com/ssaca/SsacaApplication.java
@@ -0,0 +1,13 @@
+package com.ssaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsacaApplication.class, args);
+	}
+
+}
---
+++ ssaca/src/test/java/com/ssaca/SsacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
