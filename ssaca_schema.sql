d515cecb86a8fa809435a85ae1c00e42fe9fdd87
오건영 <o54711254@ogeon-yeong-ui-MacBookAir.local>
2023-12-20 19:15:35 +0900

    feat : like테이블 추가

--- ssaca_schema.sql
+++ ssaca_schema.sql
@@ -36,8 +36,9 @@ CREATE TABLE IF NOT EXISTS `ssaca`.`board` (
   `regDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `viewCnt` INT NOT NULL DEFAULT 0,
   `price` INT NOT NULL,
-  `img` TEXT NOT NULL,
-  `orgImg` TEXT NOT NULL,
+  `img` TEXT,
+  `orgImg` TEXT,
+  `status` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_board_user_idx` (`writer` ASC) VISIBLE,
   CONSTRAINT `fk_board_user`
@@ -46,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `ssaca`.`board` (
 	ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
@@ -80,8 +82,8 @@ CREATE TABLE IF NOT EXISTS `ssaca`.`chatMessage` (
   `userId` VARCHAR(45) NOT NULL,
   `message` TEXT NOT NULL,
   `regDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `img` TEXT NOT NULL,
-  `orgImg` TEXT NOT NULL,
+  `img` TEXT,
+  `orgImg` TEXT,
   PRIMARY KEY (`id`),
   INDEX `fk_chatMessage_chatRoom1_idx` (`roomId` ASC) VISIBLE,
   CONSTRAINT `fk_chatMessage_chatRoom1`
@@ -91,7 +93,29 @@ CREATE TABLE IF NOT EXISTS `ssaca`.`chatMessage` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+CREATE TABLE IF NOT EXISTS `ssaca`.`like` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `userId` VARCHAR(45) NOT NULL,
+    `boardId` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_like_user_idx` (`userId` ASC) VISIBLE,
+    INDEX `fk_like_board_idx` (`boardId` ASC) VISIBLE,
+    CONSTRAINT `fk_like_user`
+        FOREIGN KEY (`userId`)
+        REFERENCES `ssaca`.`user` (`id`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION,
+    CONSTRAINT `fk_like_board`
+        FOREIGN KEY (`boardId`)
+        REFERENCES `ssaca`.`board` (`id`)
+        ON DELETE CASCADE
+        ON UPDATE NO ACTION)
+        ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
