78351443a30e21a06891e2f65007b544005031d0
kangjunkyu <wnsrb933@naver.com>
2023-12-22 17:03:59 +0900

    feat : test

--- ssaca_front/src/component/page/Main/BoardItem.js
+++ ssaca_front/src/component/page/Main/BoardItem.js
@@ -1,4 +1,28 @@
-const BoardItem = () => {
-  return <div></div>;
+// import "./BoardItem.module.css";
+
+const BoardItem = ({
+  id,
+  title,
+  content,
+  writer,
+  regDate,
+  viewCnt,
+  price,
+  img,
+  orgImg,
+}) => {
+  return (
+    <div className="card">
+      <img src={img} />
+      <br />
+      <div>
+        <div>{title}</div>
+        <br />
+        <div>{price}</div>
+        <br />
+        <div>{new Date(regDate).toLocaleString()}</div>
+      </div>
+    </div>
+  );
 };
 export default BoardItem;
--- ssaca_front/src/component/page/Main/BoardList.js
+++ ssaca_front/src/component/page/Main/BoardList.js
@@ -1,5 +1,19 @@
-const BoardList = () => {
-    return <div></div>;
-  };
-  export default BoardList;
-  
+import BoardItem from "./BoardItem";
+const BoardList = ({ boardList, onDetail }) => {
+  return (
+    <div>
+      <h2>게시글 리스트</h2>
+      <h4>{boardList.length}개의 게시글을 둘러보아요</h4>
+      <div className="BoardList">
+        {boardList.map((it) => (
+          <BoardItem key={it.id} {...it} onDetail={onDetail} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+BoardList.defaultProps = {
+  boardList: [],
+};
+export default BoardList;
