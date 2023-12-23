c2d7de268e6333ae033e811b728e89c909601c75
오건영 <o54711254@ogeon-yeong-ui-MacBookAir.local>
2023-12-23 19:21:04 +0900

    fix : 로그인 수정

--- ssaca/src/main/java/com/ssaca/controller/BoardRestController.java
+++ ssaca/src/main/java/com/ssaca/controller/BoardRestController.java
@@ -7,10 +7,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,6 +26,9 @@ public class BoardRestController {
     @Autowired
     BoardService boardService;
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @GetMapping("/board")
     @Operation(summary = "게시글 전체 조회")
     public ResponseEntity<?> getBoardList() {
@@ -30,7 +38,7 @@ public class BoardRestController {
 
     @GetMapping("/board/user/{userId}")
     @Operation(summary = "작성한 게시글 조회")
-    public ResponseEntity<?> getUserBoard(@PathVariable String userId){
+    public ResponseEntity<?> getUserBoard(@PathVariable String userId) {
         List<Board> result = boardService.selectUser(userId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
@@ -48,7 +56,13 @@ public class BoardRestController {
     // 게시글 등록
     @PostMapping("/board")
     @Operation(summary = "게시글 등록")
-    public ResponseEntity<?> createBoard(@RequestBody Board board) {
+    public ResponseEntity<?> createBoard(@RequestPart Board board, @RequestPart(required = false) MultipartFile file) throws IOException {
+        if (file != null && file.getSize()>0) {
+            Resource res = resourceLoader.getResource("classpath:/static/upload");
+            board.setImg(System.currentTimeMillis() + "_" + file.getOriginalFilename());
+            board.setOrgImg(file.getOriginalFilename());
+            file.transferTo(new File(res.getFile().getCanonicalFile() + "/" + board.getImg()));
+        }
         int result = boardService.insertBoard(board);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
@@ -91,7 +105,7 @@ public class BoardRestController {
     // 찜한 게시물 전체 조회
     @GetMapping("/board/wishList")
     @Operation(summary = "찜한 게시물 전체 조회")
-    public ResponseEntity<?> getWishList(String userId){
+    public ResponseEntity<?> getWishList(String userId) {
         List<Board> result = boardService.selectAllWishList(userId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
