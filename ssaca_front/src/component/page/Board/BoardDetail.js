e33c240467e73aded54a204b614d70c88c94542a
kangjunkyu <wnsrb933@naver.com>
2023-12-24 16:53:53 +0900

    feat : css 1차 수정

--- ssaca_front/src/component/page/Board/BoardDetail.js
+++ ssaca_front/src/component/page/Board/BoardDetail.js
@@ -13,15 +13,21 @@ const BoardDetail = ({ boardDetail, onEdit, onRemove }) => {
 
   return (
     <div className="BoardDetail">
-      <div>게시글 상세 내역</div>
-      <div>
-        <div>title : {title}</div>
-        <div>content : {content}</div>
-        <div>price : {price}</div>
-        <div>img : {img}</div>
+      <div className="detailTitle">게시글 상세 내역</div>
+      <div className="detailCard">
+        <div className="left">
+          <img src={img} />
+        </div>
+        <div className="right">
+          <div>상태 : 판매중</div>
+          <div>title : {title}</div>
+          <div>content : {content}</div>
+          <div>price : {price}</div>
+          <button>채팅하기</button>
+        </div>
+        <br />
       </div>
-      <br />
-      <div>
+      <div className="button">
         <button>수정</button>
         <button onClick={handleRemove}>삭제</button>
       </div>
