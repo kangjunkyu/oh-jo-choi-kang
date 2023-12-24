c6eb4a8dde3dc7822b4328a34ce06d854d71e895
kangjunkyu <wnsrb933@naver.com>
2023-12-24 17:51:17 +0900

    feat : 찜버튼 구현

--- ssaca_front/src/component/page/Board/BoardDetail.js
+++ ssaca_front/src/component/page/Board/BoardDetail.js
@@ -1,9 +1,38 @@
+import React, { useState } from "react";
+
 const BoardDetail = ({ boardDetail, onEdit, onRemove }) => {
   if (!boardDetail) {
     return <div>게시글이 선택되지 않았습니다.</div>;
   }
-
   const { id, title, content, price, img } = boardDetail;
+
+  const handleAddToWishlist = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/board/insertWishList",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId: sessionStorage.getItem("id"),
+            boardId: boardDetail.id,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        console.log("게시글을 찜했습니다.");
+      } else {
+        console.error("게시글 찜하기에 실패했습니다.");
+      }
+    } catch (error) {
+      console.error("요청 중 오류 발생", error);
+    }
+  };
 
   const handleRemove = async (id) => {
     if (window.confirm("정말로 삭제할껴?")) {
@@ -23,7 +52,10 @@ const BoardDetail = ({ boardDetail, onEdit, onRemove }) => {
           <div>title : {title}</div>
           <div>content : {content}</div>
           <div>price : {price}</div>
-          <button>채팅하기</button>
+          <div className="innerButton">
+            <button onClick={handleAddToWishlist}>찜하기</button>
+            <button>채팅하기</button>
+          </div>
         </div>
         <br />
       </div>
